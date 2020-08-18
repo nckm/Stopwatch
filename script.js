37b3ab4e3aae9f2d9f5ca81cde76c3debd4a5032
nckm <nicholas_maslin@hotmail.co.uk>
2020-08-18 11:05:03 +0100

    Implemented reset button functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+let minutes = document.getElementById('minutes')
+let seconds = document.getElementById('seconds')
+let miliseconds = document.getElementById('miliseconds')
+
+function start () {
+
+}
+
+function stop () {
+	
+}
+
+function reset () {
+	minutes.innerText = '00'
+	seconds.innerText = '00'
+	miliseconds.innerText = '00'
+}
