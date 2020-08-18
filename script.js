956724c79fe74757eb4b5687a3955bba576c3faf
nckm <nicholas_maslin@hotmail.co.uk>
2020-08-18 12:20:54 +0100

    Added javascript functionality
    - Changed onClick from HTML to javascript functions
    - start() function working
    - stop() function working
    - reset() function rewritten

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
 let minutes = document.getElementById('minutes')
 let seconds = document.getElementById('seconds')
 let miliseconds = document.getElementById('miliseconds')
+let start = document.getElementById('start')
+let stop = document.getElementById('stop')
+let reset = document.getElementById('reset')
+let interval 
+let t_minutes = 0
+let t_seconds = 0
+let t_miliseconds = 0
 
-function start () {
+start.onclick = function () {
+	interval = setInterval(startTimer, 10)
 
-}
-
-function stop () {
+	stop.onclick = function () {
+		clearInterval(interval)
+	}
 	
+	reset.onclick = function () {
+		console.log('reset')
+		t_miliseconds = 0
+		t_seconds = 0
+		t_minutes = 0
+
+		miliseconds.innerText = '0' + t_miliseconds
+		seconds.innerText = '0' + t_seconds
+		minutes.innerText = '0' + t_minutes
+	}
 }
 
-function reset () {
-	minutes.innerText = '00'
-	seconds.innerText = '00'
-	miliseconds.innerText = '00'
+function startTimer () {
+	t_miliseconds++
+
+	if (t_miliseconds < 10) {
+		miliseconds.innerText = '0' + t_miliseconds
+	} else {
+		miliseconds.innerText = t_miliseconds
+	}
+
+	if (t_miliseconds >= 99) {
+		t_miliseconds = 0
+		t_seconds ++
+
+		if (t_seconds < 10) {
+			seconds.innerText = '0' + t_seconds
+		} else {
+			seconds.innerText = t_seconds
+		}
+	}
+
+	if (t_seconds > 59) {
+		t_minutes++
+		t_seconds = 0
+		seconds.innerText = '0' + t_seconds
+		minutes.innerText = t_minutes
+
+		if (t_minutes < 10) {
+			minutes.innerText = '0' + t_minutes
+		} else {
+			minutes.innerText = t_minutes
+		}
+	}
 }
